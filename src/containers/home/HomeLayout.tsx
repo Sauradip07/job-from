11c2e7c997d520f2a11b6104a3ab26b5498e9a02
Sauradip Ghosh <sauradip96ghosh@gmail.com>
2024-09-05 20:15:37 +0530

    Controlled Form Navigation and Form Validation Across Tabs with Efficient Data Management
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>

--- src/containers/home/HomeLayout.tsx
+++ src/containers/home/HomeLayout.tsx
@@ -10,11 +10,12 @@ import {
   Box,
   Grid,
 } from "@chakra-ui/react";
-import React from "react";
+import React,{useState} from "react";
 import InterviewSettingsForm from "./InterviewSettingsForm";
 import JobDetailsForm from "./JobDetailsForm";
 import RequisitionForm from "./RequisitionDetailsForm";
 import DisplayCard from "./PreviewCard";
+import { useData } from './DataProvider'
 
 const CustomTab: React.FC<TabProps> = ({ children, ...props }) => {
   return (
@@ -25,28 +26,46 @@ const CustomTab: React.FC<TabProps> = ({ children, ...props }) => {
 };
 
 const HomeLayout = () => {
+  
+  const { state, setState } = useData()!;
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  const handleTabChange = (index: number) => {
+    setCurrentTabIndex(index);
+  };
+  const handleRequisitionSubmit = () => {
+    setCurrentTabIndex(1);
+  };
+  const handleJobDetailsSubmit = () => {
+    setCurrentTabIndex(2);
+  };
+  const handleJobDetailsPrev = () => {
+    setCurrentTabIndex(0);
+  };
+  const handlenterviewSettingsPrev = () =>{
+    setCurrentTabIndex(1);
+  }
   return (
     <Box w="100%">
       <Container maxW="1200px">
         <Heading fontFamily="Poppins" fontSize="1.5rem" my="2rem">
           Create Candidate Requisition
         </Heading>
-        <Tabs isLazy>
+        <Tabs isLazy index={currentTabIndex} onChange={handleTabChange}>
           <TabList>
-            <CustomTab>Requistion Details</CustomTab>
-            <CustomTab>Job Details</CustomTab>
-            <CustomTab>Interview Settings</CustomTab>
+            <CustomTab isDisabled={false}>Requistion Details</CustomTab>
+            <CustomTab isDisabled={state.jobDetails.jobTitle.length > 1 ? false : true}>Job Details</CustomTab>
+            <CustomTab isDisabled={state.interviewSettings.interviewLanguage.length > 1 ? false : true}>Interview Settings</CustomTab>
           </TabList>
           <Grid display="grid" gridTemplateColumns="3fr 2fr" gap="24px">
             <TabPanels>
               <TabPanel>
-                <RequisitionForm />
+                <RequisitionForm onNextTab={handleRequisitionSubmit} />
               </TabPanel>
               <TabPanel>
-                <JobDetailsForm />
+                <JobDetailsForm onNextTab={handleJobDetailsSubmit} onPrevTab={handleJobDetailsPrev}/>
               </TabPanel>
               <TabPanel>
-                <InterviewSettingsForm />
+                <InterviewSettingsForm onPrevTab={handlenterviewSettingsPrev}/>
               </TabPanel>
             </TabPanels>
             <DisplayCard />
